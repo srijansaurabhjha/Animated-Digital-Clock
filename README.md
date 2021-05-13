cc0eff4b964051fc7f6cb3711d0fcc011acec99a
srijansaurabhjha <srijanjha5@gmail.com>
2021-05-13 21:41:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-prbusu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-prbusu)
---
+++ script.js
@@ -0,0 +1,77 @@
+      function Display(){
+      let newDate = new Date();
+      let hr = newDate.getHours();
+      let min = newDate.getMinutes();
+      let sec = newDate.getSeconds();
+      document.getElementById('hr').innerHTML= hr;
+      document.getElementById('min').innerHTML= min;
+      document.getElementById('sec').innerHTML= sec;
+      }
+      setInterval(Display,1000);
+     function Toggle(){
+       let main = document.getElementById('main');
+       if(main.style.display!='none'){
+        main.style.display='none';  
+       }
+       else {
+         main.style.display='flex';
+       }
+     }
+
+    var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+//TypeWritting Animation
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
+    };
